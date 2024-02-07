537cb92a0112ced52c75cda682d3cb23e7706e8f
Poorcag 3140 <poorcag@protonmail.com>
2024-02-07 18:11:23 +1100

    added device and host pages

---
+++ host/js/main.js
@@ -0,0 +1,99 @@
+
+'use strict';
+
+let localConnection;
+let remoteConnection;
+let sendChannel;
+let receiveChannel;
+
+const startButton = document.querySelector('button#startButton');
+const closeButton = document.querySelector('button#closeButton');
+const dataField = document.getElementById('gyroscopeData')
+
+startButton.onclick = createConnection;
+closeButton.onclick = closeDataChannels;
+
+function enableStartButton() {
+    startButton.disabled = false;
+  }
+
+function createConnection() {
+    
+    window.remoteConnection = remoteConnection = new RTCPeerConnection(servers);
+    console.log('Created remote peer connection object remoteConnection');
+  
+    remoteConnection.onicecandidate = e => {
+      onIceCandidate(remoteConnection, e);
+    };
+    remoteConnection.ondatachannel = receiveChannelCallback;
+    startButton.disabled = true;
+    closeButton.disabled = false;
+}
+
+function onCreateSessionDescriptionError(error) {
+    console.log('Failed to create session description: ' + error.toString());
+}
+
+function closeDataChannels() {
+    console.log('Closing data channels');
+    sendChannel.close();
+    console.log('Closed data channel with label: ' + sendChannel.label);
+    receiveChannel.close();
+    console.log('Closed data channel with label: ' + receiveChannel.label);
+    localConnection.close();
+    remoteConnection.close();
+    localConnection = null;
+    remoteConnection = null;
+    console.log('Closed peer connections');
+    startButton.disabled = false;
+    sendButton.disabled = true;
+    closeButton.disabled = true;
+    dataChannelSend.value = '';
+    dataChannelReceive.value = '';
+    dataChannelSend.disabled = true;
+    enableStartButton();
+}
+
+function receiveChannelCallback(event) {
+    console.log('Receive Channel Callback');
+    receiveChannel = event.channel;
+    receiveChannel.onmessage = onReceiveMessageCallback;
+    receiveChannel.onopen = onReceiveChannelStateChange;
+    receiveChannel.onclose = onReceiveChannelStateChange;
+}
+  
+function onReceiveMessageCallback(event) {
+    console.log('Received Message');
+    dataField.innerText = event.data;
+}
+
+function getOtherPc(pc) {
+    return (pc === localConnection) ? remoteConnection : localConnection;
+}
+  
+function getName(pc) {
+    return (pc === localConnection) ? 'localPeerConnection' : 'remotePeerConnection';
+}
+  
+function onIceCandidate(pc, event) {
+    getOtherPc(pc)
+        .addIceCandidate(event.candidate)
+        .then(
+            onAddIceCandidateSuccess,
+            onAddIceCandidateError
+        );
+    console.log(`${getName(pc)} ICE candidate: ${event.candidate ? event.candidate.candidate : '(null)'}`);
+}
+
+function onAddIceCandidateSuccess() {
+    console.log('AddIceCandidate success.');
+}
+  
+function onAddIceCandidateError(error) {
+    console.log(`Failed to add Ice Candidate: ${error.toString()}`);
+}
+
+function onReceiveChannelStateChange() {
+    const readyState = receiveChannel.readyState;
+    console.log(`Receive channel state is: ${readyState}`);
+}
