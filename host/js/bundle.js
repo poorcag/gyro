1795542c52bf4b00cafe0a4683db66c75d32263d
Poorcag 3140 <poorcag@protonmail.com>
2024-02-08 22:24:15 +1100

    Attempting negotiation over server

--- host/js/bundle.js
+++ host/js/bundle.js
@@ -1,17 +1,38 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 'use strict';
+const QRCode = require('qrcode');
 
-const QRCode = require('qrcode')
-const sdpOffer = JSON.stringify("peerConnection.localDescription"); // Get SDP offer from WebRTC peer connection
-var canvas = document.getElementById('canvas')
+const configuration = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' }
+  ]
+};
 
-QRCode.toCanvas(canvas, sdpOffer, function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-})
+const peerConnection = new RTCPeerConnection(configuration);
 
-console.log("yes")
+// Generate SDP offer
+peerConnection.createOffer()
+  .then((offer) => {
+    return peerConnection.setLocalDescription(offer);
+  })
+  .then(() => {
+    const sdpOffer = peerConnection.localDescription;
+
+    // Generate QR code containing only the SDP offer
+    const canvas = document.getElementById('canvas');
+    QRCode.toCanvas(canvas, sdpOffer.sdp, function (error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('QR code generated successfully!');
+      }
+    });
+  })
+  .catch((error) => {
+    console.error('Error generating SDP offer:', error);
+  });
+
 
 // // Generate QR code image from SDP offer
 // qr.toDataURL(sdpOffer, { errorCorrectionLevel: 'H' }, function (err, url) {
--- host/js/main.js
+++ host/js/main.js
@@ -1,16 +1,37 @@
 
 'use strict';
+const QRCode = require('qrcode');
 
-const QRCode = require('qrcode')
-const sdpOffer = JSON.stringify("peerConnection.localDescription"); // Get SDP offer from WebRTC peer connection
-var canvas = document.getElementById('canvas')
+const configuration = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' }
+  ]
+};
 
-QRCode.toCanvas(canvas, sdpOffer, function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-})
+const peerConnection = new RTCPeerConnection(configuration);
 
-console.log("yes")
+// Generate SDP offer
+peerConnection.createOffer()
+  .then((offer) => {
+    return peerConnection.setLocalDescription(offer);
+  })
+  .then(() => {
+    const sdpOffer = peerConnection.localDescription;
+
+    // Generate QR code containing only the SDP offer
+    const canvas = document.getElementById('canvas');
+    QRCode.toCanvas(canvas, sdpOffer.sdp, function (error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('QR code generated successfully!');
+      }
+    });
+  })
+  .catch((error) => {
+    console.error('Error generating SDP offer:', error);
+  });
+
 
 // // Generate QR code image from SDP offer
 // qr.toDataURL(sdpOffer, { errorCorrectionLevel: 'H' }, function (err, url) {
